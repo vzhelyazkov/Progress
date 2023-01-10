d1853287e280d99c9817dd491b80e71b2885afe1
vzhelyzkov <92399454+vzhelyzkov@users.noreply.github.com>
2023-01-10 21:22:47 +0200

    prog bar

---
+++ script.js
@@ -0,0 +1,58 @@
+let progress = document.getElementById('progress');
+let prev = document.getElementById('prev');
+let next = document.getElementById('next');
+let circles = document.querySelectorAll('.circle');
+
+let currentActive = 1
+
+next.addEventListener('click', () => {
+    currentActive++
+
+if(currentActive > circles.length) {
+    currentActive = circles.length
+}
+
+update()
+
+})
+
+prev.addEventListener('click', () => {
+    currentActive--
+
+if(currentActive < 1) {
+    currentActive = 1
+}
+
+update()
+
+})
+
+function update() {
+    circles.forEach((circle, idx) => {
+
+if(idx < currentActive) {
+    circle.classList.add("active")
+} else {
+    circle.classList.remove("active")
+}
+
+    })
+
+    const actives = document.querySelectorAll(".active")
+
+    progress.style.width = (actives.length - 1) / (circles.length - 1) * 100 + "%"
+
+    if(currentActive === 1) {
+   prev.disabled = true;
+    } else if(currentActive === circles.length) {
+
+        next.disabled = true
+
+    } else {
+        prev.disabled = false
+        prev.disabled = false
+
+    }
+    
+}
+
